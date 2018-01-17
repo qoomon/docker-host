1727cf164fa5c75cdad9701683f8a81f66af247b
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-01-17 18:12:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 GATEWAY=$(ip route | grep '^default' | cut -d' ' -f3)
 echo "Docker Host Gateway: $GATEWAY"
 
-iptables -t nat -I PREROUTING -p tcp -j DNAT --to-destination ${GATEWAY}
+iptables -t nat -I PREROUTING -p tcp --match multiport --dports "${PORTS:-'0:65535'}" -j DNAT --to-destination ${GATEWAY}
 iptables -t nat -I POSTROUTING -j MASQUERADE
 
 # run forever
