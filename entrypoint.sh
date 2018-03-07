9e6ecfe97fe19d50078ed66281876fca386cdff3
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-03-07 13:21:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set -e
 
-macGateway="$(dig +short docker.for.mac.host.internal)"
-if [ -z "$macGateway" ]
-then
-    GATEWAY=$(ip route | grep '^default' | cut -d' ' -f3)
-else
-    GATEWAY=$macGateway
-fi        
+GATEWAY=$(ip route | grep '^default' | cut -d' ' -f3)
+
+# mac specific docker gateway
+GATEWAY_MAC="$(getent hosts docker.for.mac.host.internal | cut -d' ' -f1)"
+if [ -z "$GATEWAY_MAC" ]; then
+    GATEWAY=$GATEWAY_MAC
+fi   
+
 echo "Docker Host Gateway: $GATEWAY"
 
 iptables -t nat -I PREROUTING -p tcp --match multiport --dports "${PORTS:-'0:65535'}" -j DNAT --to-destination ${GATEWAY}
 iptables -t nat -I POSTROUTING -j MASQUERADE
 
-#run forever
+# run forever
 tail -f /dev/null
