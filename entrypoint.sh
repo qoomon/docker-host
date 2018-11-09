d87c76783b64457bb1ac3e13f6d0d869d44249d3
Bengt Brodersen <bengt.brodersen@gmail.com>
2018-11-09 16:54:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,14 @@
 set -e
 
 DOCKER_HOST="$(getent hosts host.docker.internal | cut -d' ' -f1)"
-if [ ! $DOCKER_HOST ]; then
+if [ $DOCKER_HOST ]; then
+  echo "Docker Host: $DOCKER_HOST (host.docker.internal)"
+else
   DOCKER_HOST=$(ip -4 route show default | cut -d' ' -f3)
+  echo "Docker Host: $DOCKER_HOST (default gateway)"
 fi
 
 FORWARDING_PORTS=${PORTS:-'0:65535'}
-echo "Docker Host: $DOCKER_HOST"
 
 iptables -t nat -I PREROUTING -p tcp --match multiport --dports "$FORWARDING_PORTS" -j DNAT --to-destination $DOCKER_HOST
 iptables -t nat -I POSTROUTING -j MASQUERADE
