6f1b1a1d7afa70840d6871a07541aa150f5b2e97
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-08-06 09:27:49 +0200

    replace iptable multiport match

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
+# determine docker host
 if [ $DOCKER_HOST ]; then
-  echo "Docker Host: $DOCKER_HOST (manual override)"
+  echo "Docker Host: $DOCKER_HOST"
 else
   DOCKER_HOST="$(getent hosts host.docker.internal | cut -d' ' -f1)"
   if [ $DOCKER_HOST ]; then
@@ -14,18 +15,26 @@ else
 fi
 
 FORWARDING_PORTS=${PORTS:-'0:65535'}
+echo "Forwarding ports: $FORWARDING_PORTS"
 
-iptables --table nat --insert PREROUTING --protocol tcp \
-  --match multiport --dports "$FORWARDING_PORTS" --jump DNAT --to-destination $DOCKER_HOST
-iptables --table nat --insert PREROUTING --protocol udp \
-  --match multiport --dports "$FORWARDING_PORTS" --jump DNAT --to-destination $DOCKER_HOST
-
+# setup forwarding rules
+for forwarding_port in $(echo "$FORWARDING_PORTS" | tr ";" " ")
+do
+  iptables --table nat --insert PREROUTING \
+    --protocol tcp \
+    --dport "$forwarding_port" \
+    --jump DNAT --to-destination $DOCKER_HOST
+  iptables --table nat --insert PREROUTING \
+    --protocol udp \
+    --dport "$forwarding_port" \
+    --jump DNAT --to-destination $DOCKER_HOST
+done
 iptables -t nat -I POSTROUTING -j MASQUERADE
 
+# exit on ctrl+c
 trap "exit 0;" TERM INT
 
 # Ah, ha, ha, ha, stayin' alive...
 while true; do :; done &
 kill -STOP $!
 wait $!
-
