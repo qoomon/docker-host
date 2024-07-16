af172e05677bd20d9ec9df9be5db95c6e978acd0
Bengt Brodersen <bengt.brodersen@gmail.com>
2024-07-16 12:40:15 +0200

    feat: switch to iptables-legacy due to nftables incompatiblility on windows machines

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM alpine:3.20.1
 
 RUN apk --no-cache upgrade \
  && apk --no-cache add  \
-    nftables \
+    # nftables \
+    iptables iptables-legacy \
     libcap
 
 COPY ./entrypoint.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -69,9 +69,9 @@ echo "Docker Host: $docker_host_ip ($docker_host_source)"
 PORTS="${PORTS:-"1-65535"}"
 PORTS="$(echo ${PORTS//,/ })"
 
-nft add table nat
-nft add chain nat prerouting  { type nat hook prerouting  priority -100 \; }
-nft add chain nat postrouting { type nat hook postrouting priority  100 \; }
+# nft add table nat
+# nft add chain nat prerouting  { type nat hook prerouting  priority -100 \; }
+# nft add chain nat postrouting { type nat hook postrouting priority  100 \; }
 
 echo "Forwarding ports: ${PORTS// /, }"
 for forwarding_port in $PORTS
@@ -79,14 +79,24 @@ do
   docker_container_port="${forwarding_port%%:*}"
   docker_host_port="${forwarding_port#*:}"
   
-  nft add rule nat prerouting tcp \
-    dport "${docker_container_port}" dnat to "$docker_host_ip:$docker_host_port"
-  nft add rule nat prerouting udp \
-    dport "${docker_container_port}" dnat to "$docker_host_ip:$docker_host_port"
+  # nft add rule nat prerouting tcp \
+  #   dport "${docker_container_port}" \
+  #   dnat to "$docker_host_ip:$docker_host_port"
+  iptables-legacy --table nat --insert PREROUTING \
+    --protocol tcp --destination-port "${docker_container_port/-/:}" \
+    --jump DNAT --to-destination "$docker_host_ip:$docker_host_port"   
 
+  # nft add rule nat prerouting udp \
+  #   dport "${docker_container_port}" \
+  #   dnat to "$docker_host_ip:$docker_host_port"
+  iptables-legacy --table nat --insert PREROUTING \
+    --protocol udp --destination-port "${docker_container_port/-/:}" \
+    --jump DNAT --to-destination "$docker_host_ip:$docker_host_port"
 done
 
-nft add rule nat postrouting masquerade
+# nft add rule nat postrouting masquerade
+iptables-legacy --table nat --insert POSTROUTING --jump MASQUERADE
+
 
 # --- Drop root access and "Ah, ha, ha, ha, stayin' alive" ---------------------
 
